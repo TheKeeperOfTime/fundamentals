7293f6a49c604abee353290f6a299aa4dfbc5ec7
Spencer Haggard <SpencerHaggard@Spencers-MacBook-Pro-2.local>
2016-09-21 10:29:45 -0400

    Added store project and started on JavaScript

---
+++ jsbasics/0.01_integers.js
@@ -0,0 +1,53 @@
+//console.log() prints to the console
+
+//Operations with console.log
+// console.log(6234 + 9872349);
+// console.log(2 - 7);
+// console.log(92384 * 98345);
+// console.log(10/ 5);
+
+//Modulus operator %, returns the remainder.
+// console.log(10 % 5); //returns zero
+// console.log(4 % 3); //returns a ramainder of 1
+
+//My practice with modulus operators
+// console.log(100 % 10);
+// console.log(30 % 7);
+
+var num = 20 + 20;
+
+if (num == 41) {
+	console.log("Good Job!")
+} else{
+	console.log("Sorry, try again!")
+}
+
+var a = 25;
+var b = 100;
+var c = a + b;
+console.log(a);
+console.log(b);
+console.log(c);
+
+var newNum = 10;
+newNum = (newNum) + 1;
+console.log(newNum);
+
+newNum++; // Shortcut for adding one
+console.log(newNum);
+
+newNum--;
+console.log(newNum);
+
+newNum = newNum + 10;
+console.log(newNum);
+
+newNum += 10;
+console.log(newNum);
+
+newNum--;
+console.log(newNum);
+
+newNum -= 10;
+console.log(newNum);
+
